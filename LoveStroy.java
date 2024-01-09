2e10cc26c0780ae27498a055f9d735ebe201f47a
Rajeev Ranjan <itrajeevranjan@gmail.com>
2024-01-09 19:11:51 +0530

    love stroy

--- LoveStroy.java
+++ LoveStroy.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
+
 public class LoveStroy {
     public static void main(String[] args) {
         Scanner sc = new Scanner ( System.in );
-        int n = 5;
-
+        int n = sc.nextInt ();
+        sc.nextLine ();
         String parent = "codeforces";
-        String[] arr ={
-                "coolforsez",
-                "cadafurcie",
-                "codeforces",
-                "paiuforces",
-                "forcescode"
+        String[] arr = new String[n];
 
-        };
+        //input of the Array
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextLine ();
+        }
 
         //logic
         for (int i = 0; i < n; i++) {
             int count = 0;
             for (int j = 0; j < arr[i].length (); j++) {
-                if(parent.charAt ( j ) != arr[i].charAt(j)){
+                if (parent.charAt ( j ) != arr[i].charAt ( j )) {
                     count++;
                 }
             }
-            System.out.println (count);
+            System.out.println ( count );
 
         }
     }
