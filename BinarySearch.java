7df0620747c38c2bf1fbb04949b0e9f680fe85f4
unknown <itrajeevranjan@gmail.com>
2024-01-29 00:48:28 +0530

    28 day

---
+++ BinarySearch.java
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/binary-search/submissions/1159441202/
+//Day 28
+/*
+Simply binary search I have to return the index of the target elements of the array if the elements is present int the array
+otherwise return -1
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        System.out.println(search(nums , target));
+    }
+
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+         while (start<=end){
+
+             int mid = start+(end-start)/2;
+
+             if(nums[mid] == target){
+
+                 return mid;
+
+             }
+
+             else if (nums[mid] < target){
+
+                 start = mid+1;
+
+             }
+             else {
+                 end = mid-1;
+             }
+         }
+         return -1;
+    }
+}
---
+++ InsertPosition.java
@@ -0,0 +1,52 @@
+//Day 28
+/*
+Binary search in this problem i have to return the index if the target
+ is found. If not, return the index where it would be if it were inserted in order.
+ */
+public class InsertPosition {
+    public static void main(String[] args) {
+        int[] snums = {1, 3, 5, 6};
+        int target = 5;
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+
+        if (target > nums[nums.length - 1]) {
+
+            return nums.length;
+
+        }
+
+        int start = 0;
+
+        int end = nums.length - 1;
+
+        int ans = -1;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+
+                return mid;
+
+            } else if (nums[mid] > target) {
+
+                end = mid - 1;
+
+                ans = mid;
+
+            } else {
+
+                start = mid + 1;
+
+            }
+
+        }
+
+        return ans;
+
+    }
+
+}
